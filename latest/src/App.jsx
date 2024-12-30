9008f6947561483ad8db341a373f7772dda82c7a
nishananaaah <nishanaf731@gmail.com>
2024-12-30 11:50:47 +0530

    functional and class components

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -1,37 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+
 import './App.css'
+import Mycomponent from './COMPONENTS/Greet'//you can export/import with any name
+import Welcome from './COMPONENTS/Welcome'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [color,setColor] = useState("red")
-0
+ 
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-        <div style={{background:color,width:"100px",height:"100px"}}></div>
-      <button onClick={()=>setColor('blue')}>change color</button>
+    <div>
+      <Mycomponent/>
+      <Welcome/>
+    
+    </div>
     </>
   )
 }
---
+++ latest/src/COMPONENTS/Greet.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Greet() {
+  return (
+    <div>
+      <h1>Functional Component</h1>
+    </div>
+  )
+}
+
+export default Greet // using export default we can import with any name
+// instead we use export only we give the exact name while importing
